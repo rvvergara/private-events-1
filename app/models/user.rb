3c3e3dcbe0166af28d35970381bb101ab169181e
thebigsteph <nzali.stephane86@gmail.com>
2019-06-03 22:08:31 +0100

    add association between event and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :events, foreign_key: "creator_id"
+  has_many :attendances, foreign_key: :attended_event_id
+  has_many :attended_events, through: :attendances, source: :attended_event
 end
