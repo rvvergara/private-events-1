27bd7dbc811909c27a7ed023a37bb4b281f6e6b1
Dan Montiel <danmontielh@gmail.com>
2019-06-04 13:20:14 -0500

    works associations

--- db/migrate/20190603201143_create_attendances.rb
+++ db/migrate/20190603201143_create_attendances.rb
@@ -1,8 +1,8 @@
 class CreateAttendances < ActiveRecord::Migration[5.2]
   def change
     create_table :attendances do |t|
-      t.integer :attendee_id
-      t.integer :attended_event_id
+      t.references :attendee, index: true
+      t.references :attended_event,index: true 
 
       t.timestamps
     end
